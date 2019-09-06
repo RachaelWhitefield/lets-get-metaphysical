75dcf8994bdfd1dd6c22d0264c212559b75abb6e
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-09-06 15:19:07 -0600

    added boiler plate auth login code

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Auth from "./pages/Auth";
+import NoMatch from "./pages/NoMatch";
+import TopNav from "./components/TopNav";
+import Footer from "./components/Footer";
+import { Container } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <>
+          <TopNav />
+          <Container>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/signup" render={(props) => <Auth {...props} action="signup" />} />
+              <Route exact path="/login" render={(props) => <Auth {...props} action="login" />} />
+              <Route exact path="/profile" component={Profile} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Container>
+          <Footer />
+        </>
+      </Router>
   );
 }
 
