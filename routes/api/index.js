4b4b18b168773e7e4795fc65ffe3d8bf9f0e8927
Wynn Chaney <wynn.chaney@wanco.com>
2019-09-12 21:15:22 -0600

    updating folder structure with controllers, routes, components.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const todoRoutes = require("./todoRoutes");
+const gemstoneRoutes = require("./gemstones");
 
 router.use("/users", userRoutes);
 router.use("/todos", todoRoutes);
+
+router.use("/gemstones", gemstoneRoutes);
 
 module.exports = router;
