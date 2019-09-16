7f560d0810fbcf7a6898b9dee92f540049d337aa
Scout <therearespidersinthisemail@gmail.com>
2019-09-16 13:49:44 -0600

    start of unity scripts so I have a backup and can track work

---
+++ UnityScripts/InputManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    //config params
+    private bool draggingItem = false;
+    private GameObject draggedObject;
+    private Vector2 touchOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HasInput && CompareTag("Rune")) {
+            DragOrPickUp();
+        } else {
+            if (draggingItem)
+                DropItem();
+        }
+    }
+
+    Vector2 CurrentTouchPosition {
+        get {
+            Vector2 inputPos;
+            inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            return inputPos;
+        }
+    }
+
+    private void DragOrPickUp() {
+        var inputPosition = CurrentTouchPosition;
+
+        if (draggingItem) {
+            draggedObject.transform.position = inputPosition + touchOffset;
+        } else {
+            RaycastHit2D[] touches = Physics2D.RaycastAll(inputPosition, inputPosition, 0.5f);
+            if (touches.Length > 0) {
+                var hit = touches[0];
+                if (hit.transform != null) {
+                    draggingItem = true;
+                    draggedObject = hit.transform.gameObject;
+                    touchOffset = (Vector2)hit.transform.position - inputPosition;
+                    draggedObject.transform.localScale = new Vector3(.3f,.3f,.3f);
+                }
+
+            }
+        }
+    }
+
+    private bool HasInput {
+        get {
+            return Input.GetMouseButton(0);
+        }
+    }
+
+    void DropItem() {
+        draggingItem = false;
+        draggedObject.transform.localScale = new Vector3(.25f,.25f,.25f);
+    }
+}
---
+++ UnityScripts/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+  //config params
+
+  //saving ref to scenes in order to load specific instances depending on button
+  [SerializeField] int crossScene = 1;
+  [SerializeField] int nornScene = 2;
+  [SerializeField] int singleScene = 3;
+
+  
+  public void LoadNextScene() {
+      int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+      SceneManager.LoadScene(currentSceneIndex + 1);
+  }
+
+  public void LoadCrossScene() {
+    SceneManager.LoadScene(crossScene);
+  }
+
+  public void LoadNornScene() {
+    SceneManager.LoadScene(nornScene);
+  }
+
+  public void LoadSingleScene() {
+    SceneManager.LoadScene(singleScene);
+  }
+
+  public void LoadStartScene() {
+      SceneManager.LoadScene(0);
+  }
+}
