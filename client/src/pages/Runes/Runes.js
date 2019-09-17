8ab0481bbba71f64200c6f0527440b6f01d65579
casey is on it <caseyeickhoffdesign@gmail.com>
2019-09-16 22:53:08 -0600

    started the Runespage

---
+++ client/src/pages/Runes/Runes.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Container, Col, Row } from "reactstrap";
+
+function Runes() {
+
+    return (
+        <Container>
+            <Row>
+                <Col sm={{ size: "2", offset: "2" }}>
+                    <h1>
+                        Get<br />
+                        Totally<br />
+                        RUNED
+                    </h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col sm={{ size: "6", offset: "6" }}>
+                    <h2>
+                        CASTS
+                    </h2>
+                    <Row>
+                        <Col sm="4">
+                            <img src="https://via.placeholder.com/150" />
+                        </Col>
+                        <Col sm="8">
+                        <h3>RUNIC CROSS</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec congue dolor. Sed varius augue vel massa aliquam scelerisque.</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="4">
+                            <img src="https://via.placeholder.com/150" />
+                        </Col>
+                        <Col sm="8">
+                        <h3>RUNIC CROSS</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec congue dolor. Sed varius augue vel massa aliquam scelerisque.</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="4">
+                            <img src="https://via.placeholder.com/150" />
+                        </Col>
+                        <Col sm="8">
+                        <h3>RUNIC CROSS</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec congue dolor. Sed varius augue vel massa aliquam scelerisque.</p>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Runes;
