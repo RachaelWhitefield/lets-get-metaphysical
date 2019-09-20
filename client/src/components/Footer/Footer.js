4f6cd3601e2e919c25c9a04e5db52269128205ff
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-09-20 15:09:42 -0600

    got to a good stopping point with the footer for now

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,15 +3,15 @@ import "./Footer.scss";
 
 function Footer() {
     return (
-        <footer className="footer">
+        <footer className="footer" >
     
-            <div className="casey name"><a id="linkedin" href="https://www.linkedin.com/in/rachael-whitefield-952198180/" target="_blank"><i class="fab fa-linkedin-in"></i></a>Casey Eickhoff<a id="githib" href="https://github.com/RachaelWhitefield/lets-get-metaphysical" target="blank"><i className="fab fa-github gh-icon"></i></a></div>
+            <div className="name">Casey Eickhoff<br /><a id="githib" href="https://github.com/RachaelWhitefield/lets-get-metaphysical" target="blank"><i className="fab fa-github gh-icon"></i></a><a id="linkedin" href="https://www.linkedin.com/in/rachael-whitefield-952198180/" target="_blank"><i class="fab fa-linkedin-in"></i></a></div>
 
-            <div className="rachael name"><a id="linkedin" href="https://www.linkedin.com/in/rachael-whitefield-952198180/" target="_blank"><i class="fab fa-linkedin-in"></i></a>Rachael Whitefield<a id="githib" href="https://github.com/RachaelWhitefield/lets-get-metaphysical" target="blank"><i className="fab fa-github gh-icon"></i></a></div>
-            {/* <p>STONED</p> */}
-            <div className="wynn"><a id="linkedin" href="https://www.linkedin.com/in/rachael-whitefield-952198180/" target="_blank"><i class="fab fa-linkedin-in"></i></a>Wynn Cheney<a id="githib" href="https://github.com/RachaelWhitefield/lets-get-metaphysical" target="blank"><i className="fab fa-github gh-icon"></i></a></div>
+            <div className="name">Rachael Whitefield<br /><a id="githib" href="https://github.com/RachaelWhitefield/lets-get-metaphysical" target="blank"><i className="fab fa-github gh-icon"></i></a><a id="linkedin" href="https://www.linkedin.com/in/rachael-whitefield-952198180/" target="_blank"><i class="fab fa-linkedin-in"></i></a></div>
+            
+            <div className="name">Wynn Cheney<br /><a id="githib" href="https://github.com/RachaelWhitefield/lets-get-metaphysical" target="blank"><i className="fab fa-github gh-icon"></i></a><a id="linkedin" href="https://www.linkedin.com/in/rachael-whitefield-952198180/" target="_blank"><i class="fab fa-linkedin-in"></i></a></div>
 
-            <div className="liv"><a id="linkedin" href="https://www.linkedin.com/in/rachael-whitefield-952198180/" target="_blank"><i class="fab fa-linkedin-in"></i></a>Liv Heller<a id="githib" href="https://github.com/RachaelWhitefield/lets-get-metaphysical" target="blank"><i className="fab fa-github gh-icon"></i></a></div>
+            <div className="name">Liv Heller<br /><a id="githib" href="https://github.com/RachaelWhitefield/lets-get-metaphysical" target="blank"><i className="fab fa-github gh-icon"></i></a><a id="linkedin" href="https://www.linkedin.com/in/rachael-whitefield-952198180/" target="_blank"><i class="fab fa-linkedin-in"></i></a></div>
             {/* <p></p> */}
         </footer>
     )
