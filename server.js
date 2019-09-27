9e883507c702141da59088ca868b7247ed70dd29
Scout <therearespidersinthisemail@gmail.com>
2019-09-26 18:52:12 -0600

    updated server locally to connect

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const colors = require("colors");
 const mongoose = require("mongoose");
 const routes = require("./routes");
